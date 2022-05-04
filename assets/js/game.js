4e89a8cca0fa86028edbc0a9b042baa1de9b396a
chasechri <christensonchase451@gmail.com>
2022-05-03 20:41:28 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,61 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
+console.log(playerName, playerAttack, playerHealth);
 
-console.log(10 + 10);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+//create function
+var fight = function() {
 
-console.log("Our robot's name is " + playerName)
+    window.alert("Welcome to Robot Gladiators!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    if (promptFight == "fight" || promptFight == "FIGHT") {
 
-function fight() {
-    window.alert("The fight has begun!");
+    enemyHealth = enemyHealth - playerAttack;
+
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    if(enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    if(playerHealth <= 0) {
+        window.alert(playerName + " has died.");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+}   else if (promptFight === "skip" || promptFight === "SKIP") {
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    
+    if(confirmSkip) {
+    window.alert(playerName + "has chosen to skip the fight!") ;
+    playerMoney = playerMoney - 2;
+    }
+
+    else {
+        fight()
+    }
+
+}   else {
+    window.alert("You need to choose a valid option. Try again!");
 }
 
-// fight();
+};
+
+fight();
